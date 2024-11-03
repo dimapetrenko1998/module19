b2edf5cae69437072e3cec1e23d10f0007f5ad72
dima <petrenkodima083@gmail.com>
2024-11-03 17:35:41 +0300

    1
    commit

--- GameStore/task1/models.py
+++ GameStore/task1/models.py
@@ -12,10 +12,10 @@ class Buyer(models.Model):
 
 
 class Game(models.Model):
-    title = models.CharField(max_length=200)  # Название игры
+    title = models.CharField(max_length=200)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()  # Описание
+    description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyers = models.ManyToManyField(Buyer, related_name='games')
 
