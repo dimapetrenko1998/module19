76038bb6e047887f3c36f0df7167c13f0e91638b
dima <petrenkodima083@gmail.com>
2024-11-10 19:38:59 +0300

    1
    commit

--- myproject/task1/models.py
+++ myproject/task1/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
